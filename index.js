f4a3d2bcb0e0c726ec7ae4884958d76a62e1bbad
Valentino <spinacivalentino@gmail.com>
2023-06-05 21:53:38 -0300

    Formulario contacto

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+//Formulario presupuesto
 const presupuesto = document.getElementById("presupuesto");
 const inputMetros = document.getElementById("inputMetros");
 const resultado = document.getElementById("resultado-formulario");
 
 if (presupuesto) {
-    const calcularBtn = document.getElementById("calcular");
-    calcularBtn.addEventListener("click", () => {
+    presupuesto.addEventListener("submit", (e) => {
+        e.preventDefault();
         const metros = parseInt(inputMetros.value);
         const valorPanel = 40000;
         const metrosNecesarios = metros * 0.7;
@@ -13,3 +14,39 @@ if (presupuesto) {
         inputMetros.value=" "
     });
 }
+
+
+//Formulario contacto
+const contacto = document.getElementById("contacto");
+const inputNombre = document.getElementById("nombre");
+const inputApellido = document.getElementById("apellido");
+const inputEmail = document.getElementById("email");
+const inputComentario = document.getElementById("comentario");
+
+contacto.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    if(inputNombre.value==""){
+        alert("Debes ingresar un nombre")
+    }else if(!isNaN(inputNombre.value)){
+        alert("El nombre no puede ser un número")
+    }
+
+    if(inputApellido.value==""){
+        alert("Debes ingresar un apellido")
+    }else if(!isNaN(inputNombre.value)){
+        alert("El apellido no puede ser un número")
+    }
+
+    if(inputEmail.value==""){
+        alert("Debes ingresar un mail")
+    }
+
+    if(inputComentario.value==""){
+        alert("Debes ingresar un comentario")
+    }
+
+    inputNombre.value = "" 
+    inputApellido.value = "" 
+    inputEmail.value = "" 
+    inputComentario.value = "" 
+})
